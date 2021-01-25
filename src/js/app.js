ebba10462c8571ddb2d62db348493bbbb2fabac9
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2021-01-25 20:57:32 +0300

    Fix nav icons

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
 import logo from "@/assets/logo.svg";
+import loupe from "@/assets/loupe.svg";
+import document from "@/assets/document.svg";
 import "@/css/app.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="main-container">
+        <div className="main-container__sidebar">
+            <nav className="navigation">
+                <div className="navigation__link">
+                    <img className="navigation__icon" src={loupe} />
+                </div>
+                <div className="navigation__link">
+                    <img className="navigation__icon" src={document} />
+                </div>
+            </nav>
+        </div>
+        <div className="main-container__project-structer">
+            hi how are 
+        </div>
+        <div className="main-container__open-charts">
+            you what
+        </div>
+        <div className="main-container__chart">
+            are you doing
+        </div>
+        <div className="main-container__settings">
+            are you doing
         </div>
       </div>
     );
